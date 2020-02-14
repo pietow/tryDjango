a8cf2864ca46624716bbbd16d1e353e76796b857
pietow <pprotassow@posteo.de>
2020-02-14 18:19:46 +0100

    Template engine basics

--- pages/views.py
+++ pages/views.py
@@ -4,10 +4,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "home.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
 
 
 def contact_view(*args, **kwargs):
