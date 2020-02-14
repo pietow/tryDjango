937b433fcd10c163da8b3717b0fb980b6b9a65a5
pietow <pprotassow@posteo.de>
2020-02-14 17:55:18 +0100

    django tmeplates

--- pages/views.py
+++ pages/views.py
@@ -7,7 +7,7 @@ def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
     # return HttpResponse("<h1>Hello World</h1>")
-    render(request, "home.html", {})
+    return render(request, "home.html", {})
 
 
 def contact_view(*args, **kwargs):
