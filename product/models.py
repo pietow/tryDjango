79a62757b48c3d6788311a5e9231060f401901e0
pietow <pprotassow@posteo.de>
2020-02-14 16:00:42 +0100

    field types and their para

--- product/models.py
+++ product/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField()
+    title       = models.CharField(_("title"), max_length=50)
+    description = models.TextField(blank=False, null=False) #field can be blank; blanke->rendering; null-> db-level
+    price       = models.DecimalField(_("Price"), max_digits=5, decimal_places=2)
+    summary     = models.TextField(blank=True, null=True)
+    features    = models.BooleanField(_("features"))
