02929beb7ff5af2c7b54dfd9d6057295c550cc8d
pietow <pprotassow@posteo.de>
2020-02-17 00:01:48 +0100

    delete obj

--- product/views.py
+++ product/views.py
@@ -1,10 +1,20 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductForm, rawProductForm
 # Create your views here.
 
 
+def product_delete_view(request, my_id):
+	obj = get_object_or_404(Product, id=my_id)
+	if request.method == 'POST':
+		obj.delete()
+		return redirect('../../')
+	
+	context = {
+		'object': obj
+	}
+	return render(request, "product/product_delete.html", context)
 
 def dynamic_lookup_view(request, my_id):
 	obj = get_object_or_404(Product, id=my_id)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
-from product.views import dynamic_lookup_view
+from product.views import dynamic_lookup_view, product_delete_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     #my_id is parameter of dynamic_lookup_view
     path('product/<int:my_id>', dynamic_lookup_view, name='product'),
+    path('product/<int:my_id>/delete', product_delete_view, name='product-delete'),
     path('about/', about_view),
     path('contact/', contact_view),
     path('admin/', admin.site.urls),
