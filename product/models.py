cdd23ffc363f44d794869744f60c8423e8439dac
pietow <pprotassow@posteo.de>
2020-02-16 16:09:14 +0100

    render data from the db with a model

--- product/models.py
+++ product/models.py
@@ -4,8 +4,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Product(models.Model):
-    title       = models.CharField(_("title"), max_length=50)
-    description = models.TextField(blank=False, null=False) #field can be blank; blanke->rendering; null-> db-level
-    price       = models.DecimalField(_("Price"), max_digits=5, decimal_places=2)
-    summary     = models.TextField(blank=True, null=True)
-    features    = models.BooleanField(_("features"))
+    title = models.CharField(_("title"), max_length=120)
+    # field can be blank; blanke->rendering; null-> db-level
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(_("Price"), max_digits=10000, decimal_places=2)
+    summary = models.TextField(blank=False, null=False)
+    features = models.BooleanField(_("features"))
