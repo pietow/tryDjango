d80fcc9ecdcb0a8cf397f2156b6d1eebceb281e2
pietow <pprotassow@posteo.de>
2020-02-19 19:12:54 +0100

    dynamic linking of urls

--- product/models.py
+++ product/models.py
@@ -5,8 +5,11 @@ from django.utils.translation import ugettext_lazy as _
 
 class Product(models.Model):
     title = models.CharField(_("title"), max_length=120)
-    # field can be blank; blanke->rendering; null-> db-level
+    # field can be blank; blank->rendering; null-> db-level
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(_("Price"), max_digits=10000, decimal_places=2)
     summary = models.TextField(blank=False, null=False)
     features = models.BooleanField(_("features"), default=True)
+
+    def get_absolute_url(self):
+        return f"product/{self.id}/"
